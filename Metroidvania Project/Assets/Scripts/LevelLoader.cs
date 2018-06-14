3a5be4176366d093bb156b390ca9cb2c835a0882
Noah Day <sheltielover432@gmail.com>
2018-06-14 14:18:44 -0400

    LoadingBar
    Created a loading bar for changing in between Scenes

---
+++ Metroidvania Project/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour {
+    private const string V = "%";
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+	public void LoadLevel (int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+            progressText.text = progress * 100f + V;
+            yield return null;
+        }
+    }
+}
