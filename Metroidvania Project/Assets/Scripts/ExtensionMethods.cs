4767586c6bcb5ec71f17b292f2d405f66ff22c74
Noah Day <sheltielover432@gmail.com>
2018-06-29 13:55:20 -0400

    Enemy Movement
    Created an enemy movement script based on the following video: https://www.youtube.com/watch?v=LPNSh9mwT4w&feature=youtu.be
    
    This script is meant for enemies patrolling a define area. They will pause when there's no ground or something in front of them and turn around in a confined space

---
+++ Metroidvania Project/Assets/Scripts/ExtensionMethods.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethods
+{
+    public static Vector2 toVector2(this Vector3 vector3)
+    {
+        return new Vector2(vector3.x, vector3.y);
+    }
+}
