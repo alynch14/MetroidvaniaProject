a4e90f5933901b496f6ade959063b04fbe72b431
Adam Lynch <alynch14@students.towson.edu>
2018-07-20 17:03:04 -0400

    Pushing things I have that I am not sure what I changed

--- Metroidvania Project/Assets/Scripts/PlayerMovment.cs
+++ Metroidvania Project/Assets/Scripts/PlayerMovment.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour {
 
-    GameObject player;
+    public GameObject player;
 
 	// Use this for initialization
 	void Start () {
@@ -12,18 +12,18 @@ public class PlayerMovment : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown("Right"))
+	public virtual void Update () {
+        if (Input.GetKeyDown(KeyCode.D))
         {
-            player.transform.Translate(Vector3.right);
+            player.transform.Translate(Vector3.right * 2);
         }
-        if (Input.GetKeyDown("Left"))
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            player.transform.Translate(Vector3.left);
+            player.transform.Translate(Vector3.left *(-2));
         }
-        if (Input.GetKeyDown("Space"))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            
+            player.transform.Translate(Vector3.up*2);
         }
 	}
 }
