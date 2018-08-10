7def20b7c09033b81d5c29917b5bbcce039e1c7e
Adam Lynch <alynch14@students.towson.edu>
2018-08-09 23:36:12 -0400

    Fixed script to control player and created the player character's prefab

--- Metroidvania Project/Assets/Scripts/PlayerMovment.cs
+++ Metroidvania Project/Assets/Scripts/PlayerMovment.cs
@@ -3,27 +3,113 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour {
+    // Movement ids
+    public readonly int MOVE_UP = 0;
+    public readonly int MOVE_RIGHT = 1;
+    public readonly int MOVE_DOWN = 2;
+    public readonly int MOVE_LEFT = 3;
+    public readonly int JUMP = 4;
+
+    public static KeyCode[] defaultControls =
+    {
+        KeyCode.W,
+        KeyCode.D,
+        KeyCode.S,
+        KeyCode.A,
+        KeyCode.Space
+    };
+
+    //speeds
+    public float MOVEMENT_SPEED = 2;
+    public float MOVEMENT_FRICTION = 3;
+    public float MAX_SPEED = 1;
 
     public GameObject player;
+    public Vector2 move;
+    public KeyCode[] movement;
+    public float xVel = 0;
+    public float yVel = 0;
+    private bool grounded;
 
-	// Use this for initialization
-	void Start () {
-		
+    // Use this for initialization
+    void Start () {
+        movement = defaultControls;
+        grounded = true;
 	}
-	
-	// Update is called once per frame
-	public virtual void Update () {
-        if (Input.GetKeyDown(KeyCode.D))
+
+    // Update is called once per frame
+    public void Update() {
+        float tempSpeed = MOVEMENT_SPEED;
+        float tempMax = MAX_SPEED;
+        int verticalMovement = 0;
+        int horizontalMovement = 0;
+
+        if (Input.GetKey(movement[MOVE_UP]))
         {
-            player.transform.Translate(Vector3.right * 2);
+            verticalMovement += 1;
         }
-        if (Input.GetKeyDown(KeyCode.A))
+
+        if (Input.GetKey(movement[MOVE_DOWN]))
         {
-            player.transform.Translate(Vector3.left *(-2));
+            verticalMovement -= 1;
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        if (Input.GetKey(movement[MOVE_RIGHT]))
         {
-            player.transform.Translate(Vector3.up*2);
+            horizontalMovement += 1;
         }
-	}
+
+        if (Input.GetKey(movement[MOVE_LEFT]))
+        {
+            horizontalMovement -= 1;
+        }
+
+        if (Input.GetKey(movement[JUMP]))
+        {
+            verticalMovement += 1;
+            grounded = false;
+        }
+
+        xVel += horizontalMovement * tempSpeed * Time.deltaTime;
+        if (grounded)
+        {
+            yVel = 0;
+        }
+        else
+        { 
+            yVel += verticalMovement * tempSpeed * Time.deltaTime;
+
+        }
+        if (horizontalMovement == 0)
+        {
+            xVel -= Time.deltaTime * MOVEMENT_FRICTION * Mathf.Sign(xVel);
+        }
+        if (verticalMovement == 0)
+        {
+            yVel -= Time.deltaTime * MOVEMENT_FRICTION * Mathf.Sign(yVel);
+        }
+
+        if (Mathf.Abs(xVel) > tempMax)
+        {
+            xVel = tempMax * Mathf.Sign(xVel);
+        }
+
+        if (Mathf.Abs(xVel) < 0.2f && horizontalMovement == 0)
+        {
+            xVel = 0;
+        }
+
+        if (Mathf.Abs(yVel) > tempMax)
+        {
+            yVel = tempMax * Mathf.Sign(yVel);
+        }
+
+        if (Mathf.Abs(yVel) < 0.2f && verticalMovement == 0)
+        {
+            yVel = 0;
+        }
+
+        move = (Vector2.up * yVel) + (Vector2.right * xVel);
+        gameObject.transform.Translate(move);
+    }
 }
