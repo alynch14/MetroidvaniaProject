3259bf123f29d22601e41789dfb83db11cb741fd
Noah Day <sheltielover432@gmail.com>
2018-08-10 21:10:22 -0400

    Camera controller
    Created a script for controlling the camera based from: https://answers.unity.com/questions/299102/improve-smooth-2d-side-scroller-camera-to-look-mor.html
    
    Also had some file cleanup as well, with files that were no longer needed

---
+++ Metroidvania Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;       //Public variable to store a reference to the player game object
+
+
+    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // LateUpdate is called after Update each frame
+    void Update()
+    {
+        Vector3 offset = transform.position - player.transform.position;
+        bool changeInX = false;
+        bool changeInY = false;
+        Vector3 newCameraPosition = transform.position;
+        if (offset.x > 3)
+        {
+            changeInX = true;
+            newCameraPosition.x = (player.transform.position.x + offset.x) - (offset.x - 3);
+        }
+        else if (offset.x < -3)
+        {
+            changeInX = true;
+            newCameraPosition.x = (player.transform.position.x + offset.x) - (offset.x + 3);
+        }
+        if (offset.y > 2)
+        {
+            changeInY = true;
+            newCameraPosition.y = (player.transform.position.y + offset.y) - (offset.y - 2);
+        }
+        else if (offset.y < -2)
+        {
+            changeInY = true;
+            newCameraPosition.y = (player.transform.position.y + offset.y) - (offset.y + 2);
+        }
+        if (!changeInX)
+        {
+            newCameraPosition.x = transform.position.x;
+        }
+        if (!changeInY)
+        {
+            newCameraPosition.y = transform.position.y;
+        }
+        transform.position = newCameraPosition;
+    }
+}
