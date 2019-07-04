bd88914a77ccdcc1078c9af4715811a71bc0794f
Noah Day <sheltielover432@gmail.com>
2019-07-04 16:56:48 -0400

    Working
    PlayerController Script in addition to the camera controllers seem to be working for an initial build

---
+++ Metroidvania Project/Assets/Scripts/Noah_Scripts/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;       //Public variable to store a reference to the player game object
+    public float cameraZposition = -10f;
+    public float cameraXOffset = 5f;
+    public float cameraYOffset = 1f;
+    public float horizontalSpeed = 2f;
+    public float verticalSpeed = 2f;
+    private Transform camera;
+    private Player_Control_ND playerController;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!player)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+        playerController = player.GetComponent<Player_Control_ND>();
+        camera = Camera.main.transform;
+        camera.position = new Vector3(player.transform.position.x + cameraXOffset, player.transform.position.y + cameraYOffset, player.transform.position.z + cameraZposition);
+    }
+
+    // LateUpdate is called after Update each frame
+    void Update()
+    {
+       if (playerController.isFacingRight)
+        {
+            camera.position = new Vector3(Mathf.Lerp(camera.position.x, player.transform.position.x +cameraXOffset, horizontalSpeed * Time.deltaTime), Mathf.Lerp(camera.position.y, player.transform.position.y + cameraYOffset, horizontalSpeed * Time.deltaTime), cameraZposition);
+        }
+        else
+        {
+            camera.position = new Vector3(Mathf.Lerp(camera.position.x, player.transform.position.x - cameraXOffset, horizontalSpeed * Time.deltaTime), Mathf.Lerp(camera.position.y, player.transform.position.y - cameraYOffset, horizontalSpeed * Time.deltaTime), cameraZposition);
+
+        }
+    }
+}
--- Metroidvania Project/Assets/Scripts/Noah_Scripts/Player_Control_ND.cs
+++ Metroidvania Project/Assets/Scripts/Noah_Scripts/Player_Control_ND.cs
@@ -5,6 +5,7 @@ using Prime31;
 
 public class Player_Control_ND : MonoBehaviour
 {
+    //player controls
     public CharacterController2D.CharacterCollisionState2D flags;
     public float walkSpeed = 6.0f;
     public float jumpSpeed = 8.0f;
@@ -15,6 +16,9 @@ public class Player_Control_ND : MonoBehaviour
     public float wallRunAmount = 2f;
     public float slopeSlideSpeed = 4f;
     public float glideAmount = 2f;
+    public float glideTimer = 2f;
+    
+    //player states
     public bool isGrounded;
     public bool isJumping;
     public bool isFacingRight;
@@ -23,39 +27,46 @@ public class Player_Control_ND : MonoBehaviour
     public bool isWallRunning;
     public bool isSlopeSliding;
     public bool isGliding;
+    
+    //player abilities
     public bool canDoubleJump = true;
     public bool canWallJump = true;
     public bool canWallRun = true;
     public bool canRunAfterWallJump = true;
     public bool canGlide = true;
-    private Vector3 _moveDirection = Vector3.zero;
-    private CharacterController2D _characterController;
-    private bool _lastJumpWasLeft;
-    private float _slopeAngle;
-    private Vector3 _slopeGradient = Vector3.zero;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController2D characterController;
+    private bool lastJumpWasLeft;
+    private float slopeAngle;
+    private Vector3 slopeGradient = Vector3.zero;
+    private bool startGlide;
+    private float currentGlideTimer;
     public LayerMask layerMask;
 
     // Start is called before the first frame update
     void Start()
     {
-        _characterController = GetComponent<CharacterController2D>();
+        characterController = GetComponent<CharacterController2D>();
+        currentGlideTimer = glideTimer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(wallJumped ==false)
+        characterController = GetComponent<CharacterController2D>();
+        if (wallJumped ==false)
         {
-            _moveDirection.x = Input.GetAxis("Horizontal");
-            _moveDirection.x *= walkSpeed;
+            moveDirection.x = Input.GetAxis("Horizontal");
+            moveDirection.x *= walkSpeed;
         }
 
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.up, 2f, layerMask);
         if (hit)
         {
-            _slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
-            _slopeGradient = hit.normal;
-            if (_slopeAngle > _characterController.slopeLimit)
+            slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+            slopeGradient = hit.normal;
+            if (slopeAngle > characterController.slopeLimit)
             {
                 isSlopeSliding = true;
             }
@@ -64,18 +75,19 @@ public class Player_Control_ND : MonoBehaviour
                 isSlopeSliding = false;
             }
         }
+//player is on the ground
         if (isGrounded)
         {
-            _moveDirection.y = 0;
+            moveDirection.y = 0;
             isJumping = false;
             doubleJumped = false;
-
-            if (_moveDirection.x < 0)
+            currentGlideTimer = glideTimer;
+            if (moveDirection.x < 0)
             {
                 transform.eulerAngles = new Vector3(0, 180, 0);
                 isFacingRight = false;
             }
-            else if(_moveDirection.x >0)
+            else if(moveDirection.x >0)
             {
                 transform.eulerAngles = new Vector3(0, 0, 0);
                 isFacingRight = true;
@@ -83,52 +95,73 @@ public class Player_Control_ND : MonoBehaviour
 
             if (isSlopeSliding)
             {
-                _moveDirection = new Vector3(_slopeGradient.x * slopeSlideSpeed, _slopeGradient.y * slopeSlideSpeed, 0f);
+                moveDirection = new Vector3(slopeGradient.x * slopeSlideSpeed, slopeGradient.y * slopeSlideSpeed, 0f);
             }
                 if (Input.GetButtonDown("Jump"))
             {
-                _moveDirection.y = jumpSpeed;
+                moveDirection.y = jumpSpeed;
                 isJumping = true;
                 isWallRunning = true;
             }
         }
+ //player is in the air
         else
         {
             if (Input.GetButtonUp("Jump"))
             {
-                if (_moveDirection.y > 0)
+                if (moveDirection.y > 0)
                 {
-                    _moveDirection.y = _moveDirection.y * 0.5f;
+                    moveDirection.y = moveDirection.y * 0.5f;
                 }
             }
             if (Input.GetButtonDown("Jump"))
             {
+                //double jump
                 if (canDoubleJump) {
                     if (!doubleJumped)
                     {
-                        _moveDirection.y = doubleJumpSpeed;
+                        moveDirection.y = doubleJumpSpeed;
                         doubleJumped = true;
                     }
                 }
             }
         }
-        if (canGlide ==true && Input.GetAxis("Vertical") > 0.5f && _characterController.velocity.y <0.2f)
+
+//Controls gliding for jetpack
+        if (canGlide ==true && Input.GetAxis("Vertical") > 0.5f && characterController.velocity.y <0.2f)
         {
-            isGliding = true;
-            _moveDirection.y -= glideAmount * Time.deltaTime;
+            if (currentGlideTimer >0)
+            {
+                isGliding = true;
+                if (startGlide)
+                {
+                    moveDirection.y = 0;
+                    startGlide = false;
+                }
+                moveDirection.y -= glideAmount * Time.deltaTime;
+                currentGlideTimer -= Time.deltaTime;
+            }
+            else
+            {
+                isGliding = false;
+                moveDirection.y -= gravity * Time.deltaTime;
+            }
         }
         else
         {
-            _moveDirection.y -= gravity * Time.deltaTime;
+            isGliding = false;
+            startGlide = true;
+            moveDirection.y -= gravity * Time.deltaTime;
         }
-        _moveDirection.y -= gravity * Time.deltaTime;
-        _characterController.move(_moveDirection*Time.deltaTime);
-        flags = _characterController.collisionState;
-        isGrounded = flags.below;
+
+       moveDirection.y -= gravity * Time.deltaTime;
+       characterController.move(moveDirection*Time.deltaTime);
+       flags = characterController.collisionState;
+       isGrounded = flags.below;
 
         if (flags.above)
         {
-            _moveDirection.y -= gravity * Time.deltaTime;
+            moveDirection.y -= gravity * Time.deltaTime;
         }
         if (flags.left || flags.right)
         {
@@ -136,7 +169,7 @@ public class Player_Control_ND : MonoBehaviour
             {
                 if (Input.GetAxis("Vertical") >0 && isWallRunning == true)
                 {
-                    _moveDirection.y = jumpSpeed / wallRunAmount;
+                    moveDirection.y = jumpSpeed / wallRunAmount;
                     StartCoroutine(WallRunWaiter());
                 }
             }
@@ -144,19 +177,19 @@ public class Player_Control_ND : MonoBehaviour
             {
                 if (Input.GetButtonDown("Jump") || wallJumped== false && isGrounded==false)
                 {
-                    if(_moveDirection.x < 0)
+                    if(moveDirection.x < 0)
                     {
-                        _moveDirection.x = jumpSpeed * wallJumpXAmount;
-                        _moveDirection.y = jumpSpeed * wallJumpYAmount;
+                        moveDirection.x = jumpSpeed * wallJumpXAmount;
+                        moveDirection.y = jumpSpeed * wallJumpYAmount;
                         transform.eulerAngles = new Vector3(0, 180, 0);
-                        _lastJumpWasLeft = false;
+                        lastJumpWasLeft = false;
                     }
-                    else if (_moveDirection.x > 0)
+                    else if (moveDirection.x > 0)
                     {
-                        _moveDirection.x = jumpSpeed * wallJumpXAmount;
-                        _moveDirection.y = jumpSpeed * wallJumpYAmount;
+                        moveDirection.x = jumpSpeed * wallJumpXAmount;
+                        moveDirection.y = jumpSpeed * wallJumpYAmount;
                         transform.eulerAngles = new Vector3(0, 0, 0);
-                        _lastJumpWasLeft = true;
+                        lastJumpWasLeft = true;
                     }
                     StartCoroutine(WallJumpWaiter());
                 }
@@ -171,12 +204,14 @@ public class Player_Control_ND : MonoBehaviour
             }
         }
     }
+    //timer for wall jumping
     IEnumerator WallJumpWaiter()
     {
         wallJumped = true;
         yield return new WaitForSeconds(0.5f);
         wallJumped = false;
     }
+    //timer for wall running
     IEnumerator WallRunWaiter()
     {
         isWallRunning = true;
